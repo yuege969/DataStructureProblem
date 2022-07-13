80b71ba1f9c7d5f53fd9305a4634c51bdac7fd2e
yuege969 <yuege_969@163.com>
2022-07-13 09:01:22 +0800

    更新12题，增加时间复杂度与空间复杂度

--- 第2章 线性表/PastExamPaper12/PastExamPaper12/PastExamPaper12.cpp
+++ 第2章 线性表/PastExamPaper12/PastExamPaper12/PastExamPaper12.cpp
@@ -3,18 +3,21 @@
 * 题意分析：找出数组中出现次数大于n/2的元素,n为数组长度
 * 算法思路：目标元素的数量一定比剩余的其他所有的元素的数量多
 * 遍历整个数组的所有元素，标记出一个可能是主元素的数；
-* 在遍历一遍进行验证
+* 再遍历一遍进行验证
+* 时间复杂度：O(n)
+* 空间复杂度：O(1)
 */
 #include<iostream>
 using namespace std;
 int findMain(int A[], int n) {
-	int cnt = 0, x = -1;
+	int cnt = 0, x = -1;//计数器,所标记的目标元素
 	for (int i = 0;i < n;i++) {
 		if (cnt == 0) x = A[i], cnt = 1;
 		else if (cnt != 0 && x != A[i]) cnt--;
 		else if (cnt != 0 && x == A[i]) cnt++;
 	}
 	if (cnt) {
+		//再次确认所标记的元素是否为目标元素
 		int c = 0;
 		for (int i = 0;i < n;i++) {
 			if (A[i] == x) c++;
